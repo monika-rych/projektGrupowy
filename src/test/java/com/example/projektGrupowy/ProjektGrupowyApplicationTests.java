61c7330d959ac184471d4b56af34834ce81df6b0
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-05-21 20:22:07 +0100

    Initial commit

---
+++ src/test/java/com/example/projektGrupowy/ProjektGrupowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektGrupowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektGrupowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
